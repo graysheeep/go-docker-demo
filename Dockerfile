fa41b17d659c4f638e11aba18e0032e016dfd681
gray_sheep <crazy_egg2009@qq.com>
2018-04-26 18:33:29 +0800

    enable helloworld web

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,3 @@
 FROM daocloud.io/golang:1.3-onbuild
+
+EXPOSE 8080
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
-import "fmt"
-func main(){
-    fmt.Println("hello world")
+import (
+    "fmt"
+    "net/http"
+)
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
+}
+func main() {
+    http.HandleFunc("/World", handler)
+    http.ListenAndServe(":8080", nil)
 }
